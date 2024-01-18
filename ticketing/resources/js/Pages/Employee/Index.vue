59acb8ed44ddb8e5cfc5c96a7a8e73575e9a5f08
Prince Taguiling <2200326@slu.edu.ph>
2024-01-18 10:49:37 +0800

    updated employee index

--- ticketing/resources/js/Pages/Employee/Index.vue
+++ ticketing/resources/js/Pages/Employee/Index.vue
@@ -1,14 +1,41 @@
 <template>
     <Header></Header>
-    <div class="search">
+    <br>
 
+    <div class="search">
+        <input
+        type="text"
+        v-model="searchQuery"
+        placeholder="Search Tickets..."
+        @input="handleSearch"
+      />
     </div>
 
-    <div class="container text-center w-100 h-100 justify-center bg-warning">
-        test
+    <div class="container text-center w-100 h-100 justify-center">
+        <H1>View All Tickets</H1>
+        <p> Manage and Track all TMDD tickets</p>
+        <button>Create New ticket</button>
+        <br><br>
+        <button>All</button>
+        <button>New</button>
+        <button>Pending</button>
+        <button>Resolved</button>
+    </div>
+    <div class="table-container">
+
     </div>
 </template>
 <script setup>
 import Header from "@/Pages/Layouts/Header.vue"
 </script>
+
+
+<style>
+* {
+  font-family: 'Poppins', sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
 
--- ticketing/resources/js/Pages/Layouts/Header.vue
+++ ticketing/resources/js/Pages/Layouts/Header.vue
@@ -1,13 +1,80 @@
 <template>
-    <H1>TESOIIG</H1>
     <nav>
-        <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Contact</a>
+        <div class="left">
+            <a href="#" class="logo"></a>
+            <a href="#" class="brand">TMDD Ticketing System</a>
+            </div>
+            <div class="right">
+            <a href="#" class="profile-picture"></a>
+            <a href="#" class="user-link">Employee</a>
+        </div>
     </nav>
 
 </template>
 
 <script>
 </script>
+
+
+<style>
+:root {
+  /* COLORS */
+  --primary-color: #e8e8e8;
+  --text-color: #282828;
+}
+
+* {
+  font-family: 'Poppins', sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+nav {
+  background-color: var(--primary-color);
+  color: #fff;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.left, .right {
+  display: flex;
+  align-items: center;
+}
+
+.logo, .profile-picture {
+  width: 30px;
+  height: 30px;
+  background-color: #ffffff; /* Gray color */
+  display: block;
+  margin-right: 10px;
+  border-radius: 50%; /* Make it rounded */
+}
+
+.brand, .user-link {
+  color: var(--text-color);
+  text-decoration: none;
+  font-weight: bold;
+  font-size: 16px;
+}
+
+.user-link {
+  padding: 8px 15px;
+  border-radius: 5px;
+  
+  transition: background-color 0.3s;
+}
+
+.user-link:hover {
+  background-color: #777; /* Background color on hover */
+}
+
+.right .profile-picture {
+  margin-right: 0; /* Remove margin for the profile picture */
+}
+
+.right .user-link {
+  margin-right: 10px; /* Add margin for user link */
+}
+</style>
