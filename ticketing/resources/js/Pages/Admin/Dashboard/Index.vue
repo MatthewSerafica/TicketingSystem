716015e7ae4aaf8385f9398dbd10c2e871f97fe4
matthewserafica01 <121957248+matthewserafica01@users.noreply.github.com>
2024-01-18 10:21:41 +0800

    modify index.vue

--- ticketing/resources/js/Pages/Admin/Dashboard/Index.vue
+++ ticketing/resources/js/Pages/Admin/Dashboard/Index.vue
@@ -1,8 +1,10 @@
 <template>
+    <Header></Header>
     <Button></Button>
 </template>
 <script setup>
 import Button from '@/Components/Button.vue'
+import Header from '@/Pages/Layouts/Header.vue'
 </script>
 
 <style></style>
