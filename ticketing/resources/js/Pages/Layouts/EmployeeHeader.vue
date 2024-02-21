08d8dffc970ebb612e4ff94d6a65f32865589300
matthewserafica01 <121957248+matthewserafica01@users.noreply.github.com>
2024-02-21 11:03:27 +0800

    modify employeeheader

--- ticketing/resources/js/Pages/Layouts/EmployeeHeader.vue
+++ ticketing/resources/js/Pages/Layouts/EmployeeHeader.vue
@@ -1,74 +1,63 @@
 <template>
-  <nav>
-    <div class="left">
-      <a href="/employee" class="logo"></a>
-      <a href="/employee" class="brand">TMDD Ticketing System</a>
-    </div>
-    <div class="right">
-      <a href="#" class="profile-picture"></a>
-      <a href="#" class="user-link">Employee</a>
-    </div>
+  <nav class="navbar navbar-expand-lg shadow-sm header-color">
+      <div class="container-fluid gap-3">
+          <div class="d-flex gap-2 col-8">
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                  class="bi bi-brilliance mt-2" viewBox="0 0 16 16">
+                  <path
+                      d="M8 16A8 8 0 1 1 8 0a8 8 0 0 1 0 16M1 8a7 7 0 0 0 7 7 3.5 3.5 0 1 0 0-7 3.5 3.5 0 1 1 0-7 7 7 0 0 0-7 7" />
+              </svg>
+              <a class="navbar-brand text-white" href="/technician">TMDD Ticketing System</a>
+          </div>
+          <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button> -->
+          <!-- <div class="" id="navbarNav">
+              <ul class="navbar-nav">
+                  <li class="nav-item">
+                      <a class="nav-link text-white" aria-current="page" href="/technician">Dashboard</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link text-white" href="/technician/tickets">Tickets</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link text-white" href="/technician/service-report">Service Reports</a>
+                  </li>
+              </ul>
+          </div> -->
+          <div class="d-flex gap-2 pe-5 me-5 justify-content-center align-items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                  class="bi bi-person-circle" viewBox="0 0 16 16">
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                  <path fill-rule="evenodd"
+                      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+              </svg>
+              <div class="dropdown-center">
+                  <a class="text-decoration-none dropdown-toggle text-white" type="button" data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      {{ page.props.user.name }}
+                  </a>
+                  <ul class="dropdown-menu">
+                      <li>
+                          <Link :href="route('logout')" method="delete" v-if="page.props.user"
+                              class="text-decoration-none dropdown-item">Logout
+                          </Link>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </div>
   </nav>
 </template>
 
-<script>
+<script setup>
+import { Link, usePage } from "@inertiajs/vue3";
+
+const page = usePage();
 </script>
 
-
 <style>
-nav {
-  background-color: var(--primary-color);
-  color: #fff;
-  padding: 10px;
-  display: flex;
-  justify-content: space-between;
-
+.header-color {
+  background-color: #063970;
 }
-
-.left,
-.right {
-  display: flex;
-  align-items: center;
-}
-
-.logo,
-.profile-picture {
-  width: 30px;
-  height: 30px;
-  background-color: #ffffff;
-  /* Gray color */
-  display: block;
-  margin-right: 10px;
-  border-radius: 50%;
-  /* Make it rounded */
-}
-
-.brand,
-.user-link {
-  color: var(--text-color);
-  text-decoration: none;
-  font-weight: bold;
-  font-size: 16px;
-}
-
-.user-link {
-  padding: 8px 15px;
-  border-radius: 5px;
-
-  transition: background-color 0.3s;
-}
-
-.user-link:hover {
-  background-color: #777;
-  /* Background color on hover */
-}
-
-.right .profile-picture {
-  margin-right: 0;
-  /* Remove margin for the profile picture */
-}
-
-.right .user-link {
-  margin-right: 10px;
-  /* Add margin for user link */
-}</style>
+</style>
