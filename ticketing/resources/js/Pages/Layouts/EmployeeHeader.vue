2d555a057052ea00c00139163942865c57176328
LOVELY OSNGAL <2201189@slu.edu.ph>
2024-02-21 14:25:42 +0800

    Bell Icon

--- ticketing/resources/js/Pages/Layouts/EmployeeHeader.vue
+++ ticketing/resources/js/Pages/Layouts/EmployeeHeader.vue
@@ -9,23 +9,8 @@
                 </svg>
                 <Link class="navbar-brand text-white" :href="`/employee`">TMDD Ticketing System</Link>
             </div>
-            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button> -->
-            <!-- <div class="" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link text-white" aria-current="page" href="/technician">Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="/technician/tickets">Tickets</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="/technician/service-report">Service Reports</a>
-                    </li>
-                </ul>
-            </div> -->
             <div class="d-flex gap-2 pe-5 me-5 justify-content-center align-items-center">
+                <i class="bi bi-bell text-white me-3"  style="font-size: 20px;"></i> 
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
                     class="bi bi-person-circle" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
