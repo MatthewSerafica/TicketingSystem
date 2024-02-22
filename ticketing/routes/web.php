727ca649983d21315b79931767b71508b2651591
matthewserafica01 <121957248+matthewserafica01@users.noreply.github.com>
2024-02-22 11:32:21 +0800

    modify routing

--- ticketing/routes/web.php
+++ ticketing/routes/web.php
@@ -44,10 +44,10 @@ Route::middleware(['web'])->group(function () {
     });
 
     Route::middleware(['auth', 'technician'])->group(function () {
-        Route::get('/technician', [TechnicianDashboardController::class, 'index']);
-        Route::get('/technician/tickets', [TechnicianTicketController::class, 'index']);
-        Route::get('/technician/tickets/create', [TechnicianTicketController::class, 'create']);
-        Route::get('/technician/tickets/create/store', [TechnicianTicketController::class, 'store']);
+        Route::get('/technician', [TechnicianDashboardController::class, 'index'])->name('technician');
+        Route::get('/technician/tickets', [TechnicianTicketController::class, 'index'])->name('technician.tickets');
+        Route::get('/technician/tickets/create', [TechnicianTicketController::class, 'create'])->name('technician.tickets.create');
+        Route::get('/technician/tickets/create/store', [TechnicianTicketController::class, 'store'])->name('technician.tickets.store');
         Route::get('/technician/service-report', [TechnicianServiceController::class, 'index']);
         Route::get('/technician/service-report/create', [TechnicianServiceController::class, 'create']);
     });
