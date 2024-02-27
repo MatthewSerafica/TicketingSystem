80a11070dbe6d2f8baa3ba56b04f6d02f8a75b98
PrawnsJas <156972633+PrawnsJas@users.noreply.github.com>
2024-02-27 14:40:41 +0800

    debugging delete modal

--- ticketing/resources/js/Components/DeleteModal.vue
+++ ticketing/resources/js/Components/DeleteModal.vue
@@ -1,28 +1,27 @@
 <template>
-    <div class="modal fade" tabindex="-1" aria-labelledby="deleteModalLabel" >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="deleteModalLabel"><!-- {{ title }} --></h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <!-- {{ message }} -->
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-            <button type="button" class="btn btn-danger" @click="confirmAction">Delete</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </template>
-  
-  <script setup>
-  
-  </script>
-  
-  <style scoped>
-  /* Add your custom styles for the modal */
-  </style>
-  
+ 
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+
+const { modal, hide } = useModal();
+const modalId = 'deleteModal'; // You can customize the modal ID here
+
+const title = ref('');
+const message = ref('');
+const confirmAction = ref(() => {});
+
+const showModal = (modalTitle, modalMessage, action) => {
+  title.value = modalTitle;
+  message.value = modalMessage;
+  confirmAction.value = action;
+  modal.show(modalId);
+};
+
+const hideModal = () => {
+  modal.hide(modalId);
+};
+
+</script>
