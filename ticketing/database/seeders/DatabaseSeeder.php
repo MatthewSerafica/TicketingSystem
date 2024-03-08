2565a03d5b684408fb1ede5e4c4e325c5061c5c2
adrian <2212992@slu.edu.ph>
2024-03-08 12:05:36 +0800

    database seeder

--- ticketing/database/seeders/DatabaseSeeder.php
+++ ticketing/database/seeders/DatabaseSeeder.php
@@ -19,30 +19,6 @@ class DatabaseSeeder extends Seeder
                 'password' => '123456',
                 'user_type' => 'admin',
                 ],
-                [
-                'name' => 'Adrian Salinas',
-                'email' => 'adriansalinas@slu.edu.ph',
-                'password' => '123456',
-                'user_type' => 'employee',
-                ],
-                [
-                'name' => 'Matthew Serafica',
-                'email' => 'matthewserafica@slu.edu.ph',
-                'password' => '123456',
-                'user_type' => 'technician',
-                ],
-                [
-                'name' => 'Lovely Osngal',
-                'email' => 'lovelyosngal@slu.edu.ph',
-                'password' => '123456',
-                'user_type' => 'technician',
-                ],
-                [
-                'name' => 'Prince Taguiling',
-                'email' => 'princetaguiling@slu.edu.ph',
-                'password' => '123456',
-                'user_type' => 'employee',
-                ],
                     
             ];
 
@@ -50,7 +26,7 @@ class DatabaseSeeder extends Seeder
             \App\Models\User::factory()->create($user);
         }
 
-        $employees = [
+        /* $employees = [
             [
             'user_id' => '2',
             'department' => 'SEA',
@@ -66,9 +42,9 @@ class DatabaseSeeder extends Seeder
 
         foreach ($employees as $employee) {
             \App\Models\Employee::factory()->create($employee);
-        }
+        } */
 
-            $technicians = [
+            /* $technicians = [
                 [
                 'user_id' => '3'
                 ],
@@ -80,60 +56,25 @@ class DatabaseSeeder extends Seeder
 
         foreach ($technicians as $technician) {
             \App\Models\Technician::factory()->create($technician);
-        }
+        } */
 
             
-            $tickets = [
-                [
-                'employee' => '1',
-                'technician1' => '1',
-                'issue' => 'Hardware Issue',
-                'service' => 'Hardware Repair',
-                'description' => 'broken monitor',
-                'status' => 'Pending',
-                ],
-                [
-                'employee' => '2',
-                'issue' => 'Network Issue',
-                'description' => 'No internet connection',
-                'status' => 'New',
-                'service' => 'Software Troubleshoot',
-                ],
-                [
-                'employee' => '2',
-                'technician1' => '1',
-                'technician2' => '2',
-                'issue' => 'Network Issue',
-                'description' => 'No internet connection',
-                'status' => 'New',
-                'service' => 'Software Troubleshoot',
-                ],
+            /* $tickets = [
+                
                     
             ];
         foreach ($tickets as $ticket) {
             \App\Models\Ticket::factory()->create($ticket);
-        }
+        } */
 
-        $service_reports = [
+        /* $service_reports = [
             [
-                'service_id' => '0001',
-                'date_started' => '2022-02-22',
-                'time_started' => '08:00:00',
-                'ticket_number' => 1,
-                'technician' => '1',
-                'requesting_office' => 'IT Department',
-                'equipment_no' => 'E123',
-                'issue' => 'Network connectivity problem',
-                'action' => 'Checked cables and fixed the issue',
-                'recommendation' => 'Install additional network equipment',
-                'date_done' => '2022-02-22',
-                'time_done' => '10:00:00',
-                'remarks' => 'Done',
+                
             ],
         ];
         foreach ($service_reports as $service_reports) {
             \App\Models\ServiceReport::factory()->create($service_reports);
-        }
+        } */
 
         $departments =[
             [
