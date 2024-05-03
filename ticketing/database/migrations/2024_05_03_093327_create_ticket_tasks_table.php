c773156344040d6cc3ef0ce5f510c9892ca000d2
matthewserafica01 <121957248+matthewserafica01@users.noreply.github.com>
2024-05-03 09:42:25 +0800

    added ticket_tasks database and model

---
+++ ticketing/database/migrations/2024_05_03_093327_create_ticket_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ticket_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('ticket_number');
+            $table->foreign('ticket_number')->references('ticket_number')->on('tickets')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('user')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('task_name');
+            $table->boolean('is_resolved');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ticket_tasks');
+    }
+};
